5d01f0942a09eaf20bf1ef011fc551322842b399
kongfanqin <kongfq@buyelec.net>
2024-12-24 18:06:54 +0800

    redux异步加载用户信息

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import { useRoutes } from 'react-router-dom'
-import routes from '@/router'
-const App = () => {
+import React,{Suspense,useEffect} from 'react'
+// 导入路由及react-redux钩子
+import { useNavigate, useRoutes } from 'react-router-dom'
+// 导入接口
+import { useDispatch, useSelector } from 'react-redux'
+import { getUserInfoAsync } from "./store/reducers/userSlice";
+import { generateRoutes } from "./store/reducers/permissionSlice";
+import { getToken } from "./utils/auth";
+//导入loading组件
+import Loading from "./components/Loading";
+
+export default function App() {
+  const dispatch = useDispatch()
+  const routes = useSelector((state) => state.permission.routes)
+  //跳转方法
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // 从本地获取token
+      const token = getToken()
+      // 如果token存在，说明用户已登录
+      if (token) {
+        // 调用getUserInfoAsync获取用户信息
+        await dispatch(getUserInfoAsync())
+        // 调用generateRoutes获取路由信息
+        await dispatch(generateRoutes())
+      } else {
+        navigate('/login',{ replace: true})
+      }
+    }
+    fetchData()
+  }, [dispatch])
+  // 利用hook转换路由表
   const element = useRoutes(routes)
   return (
-    <div>{element}</div>
+      <>
+        <Suspense fallback={<Loading />}>
+          {routes && element}
+        </Suspense>
+      </>
+
   )
 }
-export default App
--- src/store/reducers/userSlice.js
+++ src/store/reducers/userSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { getRefreshToken, getToken, removeRefreshToken, removeToken, setRefreshToken, setToken } from '@/utils/auth'
 import userApi from '@/api/user'
+import { generateRoutes } from './permissionSlice'
 
 /**
  * 创建一个用户状态切片
@@ -22,10 +23,38 @@ const userSlice = createSlice({
     setUserinfo(state, action) {
       const { payload } = action
         state.userinfo = payload
+    },
+    login(state, action) {
+      state.token = action.payload.token
+      state.refreshToken = action.payload.refreshToken
+      // 将数据持久化
+      setToken(state.token)
+      setRefreshToken(state.refreshToken)
+    },
+    logout(state, action) {
+      state.token = null
+      state.refreshToken = null
+      state.userinfo = null
+      // 移除存储中的信息
+      removeToken()
+      removeRefreshToken()
     }
   }
 })
 // 导出经过redux包装的action对象
-export const { setUserinfo } = userSlice.actions
+export const { login, setUserinfo,logout } = userSlice.actions
+// 导出获取登录用户信息的异步方法
+export const getUserInfoAsync = () => async (dispatch) => {
+  const { data } = await userApi.center.get()
+  dispatch(setUserinfo({ ...data, avatar: data.avatar ? process.env.React_APP_IMG_API + '/' + data.avatar : null }))
+  return data
+}
+// 登录异步方法
+export const loginAsync = (payload) => async (dispatch) => {
+  const { data } = await userApi.login.login(payload)
+  dispatch(login(data))
+  const userinfo = await dispatch(getUserInfoAsync())
+  dispatch(generateRoutes(userinfo.menus))
+}
 // 导出切片对象
 export default userSlice
